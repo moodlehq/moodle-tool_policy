59ebf2f091a0fa24d682a214e322991be7a590b5
David Mudrák <david@moodle.com>
2018-03-19 15:36:35 +0100

    Call it version 33.0.0 for Moodle 3.3

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'tool_policy';
-$plugin->release = '0.5.0';
-$plugin->version = 2017051508;
+$plugin->release = '33.0.0';
+$plugin->version = 2017051509;
 $plugin->requires = 2017051505;
-$plugin->maturity = MATURITY_ALPHA;
+$plugin->maturity = MATURITY_BETA;
