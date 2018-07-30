9b42dfdf48e1c2c69930b17a8b6f22d889de3845
sarjona <sara@moodle.com>
2018-07-30 12:34:27 +0200

    Call it 33.8.0 [ci skip]

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'tool_policy';
-$plugin->release = '33.7.0';
-$plugin->version = 2017051518;
+$plugin->release = '33.8.0';
+$plugin->version = 2017051519;
 $plugin->requires = 2017051505;
 $plugin->maturity = MATURITY_STABLE;
